b81b6f3e6895e4b74cc10df13c233e5e306e8aaa
Andrew Jordan <acjordan2@gmail.com>
2015-04-28 21:50:20 -0700

    Added basic functionality to add clients

--- ssoauth/views/client.py
+++ ssoauth/views/client.py
@@ -5,23 +5,27 @@ from models import db, Client
 
 client = Blueprint('client', __name__)
 
-@client.route('/client/add')
+@client.route('/client/add', methods=['GET', 'POST'])
 @login_required
 def addClient():
-    item = Client(
-        client_id = gen_salt(40),
-        client_secret = gen_salt(50),
-        client_name = "Test OAuth Client",
-        client_description = "OAuthclient Used for testing purposes",
-        _redirect_uris = ' '.join([
-            'http://localhost:8000/authorized',
-        ]),
-        _default_scopes='email',
-        user_id = current_user.id
-    )
-    db.session.add(item)    
-    db.session.commit()
-    return jsonify(
-        client_id = item.client_id,
-        client_secret = item.client_secret
-    )
+    if request.method == 'GET':
+        return render_template("client/add.html")     
+    else:
+        item = Client(
+            client_id = gen_salt(40),
+            client_secret = gen_salt(50),
+            client_name = "Test OAuth Client",
+            client_description = "OAuthclient Used for testing purposes",
+            _redirect_uris = ' '.join([
+                'http://localhost:8000/authorized',
+            ]),
+            _default_scopes='email',
+            user_id = current_user.id
+        )
+        db.session.add(item)    
+        db.session.commit()
+        return jsonify(
+            client_id = item.client_id,
+            client_secret = item.client_secret
+        )
+
